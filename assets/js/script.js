636ace30bcb45a2de91cc0a40553927c99a6ed85
Vishal Tarwatkar <vishaltarwatkar321@gmail.com>
2024-01-15 22:46:59 +0530

    home animation complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,69 @@
-let tl = gsap.timeline();
+//text transform animation
+//this will help us in a way that we dont have to create html elements manually
+//it will automatically get added in those text which has "reveal" class
+function revealToSpan() {
+   document.querySelectorAll(".reveal").forEach(function (elem) {
+      //this code creates two span tags
+      let Parent = document.createElement("span");
+      let Child = document.createElement("span");
 
-tl
-.to(".mainPage", {
+      //this code give both parent and chil span their respective classes
+      Parent.classList.add("parent");
+      Child.classList.add("child");
+      // <span class="parent"></span>
+      // <span class="child"></span>
+
+      //span prarent gets child and clid gets elem properties
+      //this code give every child content the detail/data of elem
+      Child.innerHTML = elem.innerHTML;
+      //this line just simply append the child to parent
+      Parent.appendChild(Child);
+      // <span class="parent"><span class="child"></span></span>
+
+      elem.innerHTML = "";
+      elem.appendChild(Parent);
+   });
+}
+
+revealToSpan();
+
+//GSAP Animation
+let tl = gsap.timeline();
+//text sliding(left) animation
+tl.from(".child span", {
+   x: 200,
+   delay: .5,
+   stagger: .2,
+   duration: 1.4,
+   ease: Power3.easeInOut
+});
+
+//text sliding(up) animation
+tl.to(".parent .child", {
+   y: "-100%",
+   duration: 1.5,
+   delay: 0.2,
+   ease: Circ.easeInOut
+});
+
+//page sliding animation
+tl.to(".loader", {
    height: 0,
-   duration: 2,
+   duration: 1,
    ease: Expo.easeInOut,
 });
-tl.to(".emptyDiv1", {
+
+tl.to(".green", {
    height: "100%",
-   duration: 2,
-   delay:-2,
+   top: 0,
+   duration: 1,
+   delay: -1,
    ease: Expo.easeInOut,
 });
-tl.to(".emptyDiv2", {
-   height: "100%",
-   duration: 2,
-   delay:-1.8,
+
+tl.to(".green", {
+   height: "0%",
+   duration: 1,
+   delay: - .2,
    ease: Expo.easeInOut,
 });
